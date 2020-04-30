0703ede6995ac860212db6485f0666364a7ce5dd
Mikhail Semin <mikhail.semin@emlid.com>
2020-04-30 14:02:55 +0300

    add movie's genres fetching

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div
-    class="container mx-auto"
+    class="container mx-auto p-5"
     id="app"
   >
     <div id="nav">
-      <router-link to="/">Movie Finder</router-link>
+      <router-link
+        class="text-2xl"
+        to="/"
+      >
+        The MovieDB Finder
+        <span class="text-base">(Alpha 0.1)</span>
+      </router-link>
     </div>
     <keep-alive :exclude="['Movie']">
       <router-view />
@@ -14,11 +20,12 @@
 
 <script>
   export default {
-    props: ['configuration'],
+    props: ['configuration', 'genres'],
 
     provide() {
       return {
         configuration: this.configuration,
+        genres: this.genres,
       };
     },
   };
@@ -26,6 +33,7 @@
 
 <style>
   body {
+    background-color: yellow;
     font-size: 16px;
   }
   #app {
--- src/main.js
+++ src/main.js
@@ -8,9 +8,10 @@ Vue.config.productionTip = false;
 
 (async () => {
   const configuration = (await api('/configuration')).data;
+  const { genres } = (await api('/genre/movie/list')).data;
 
   return new Vue({
     router,
-    render: h => h(App, { props: { configuration }}),
+    render: h => h(App, { props: { configuration, genres: [...genres] } }),
   }).$mount('#app');
 })();
